b97a06ce714410cf38c25a18fdcb4fb990f01587
ilya <ilya.nikokoshev@gmail.com>
2015-04-28 11:30:16 +0300

    Initial Commit

---
+++ VideoSampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoSampler
+//
+//  Created by Ilya Nikokoshev on 28/04/15.
+//  Copyright (c) 2015 ilya n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
