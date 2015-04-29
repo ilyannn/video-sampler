ef3f13734932a2385d5342d475fa45b309071d25
ilya <ilya.nikokoshev@gmail.com>
2015-04-30 01:08:03 +0300

    can't use c++ in swift :(

--- VideoSampler/VideoSampler-Bridging-Header.h
+++ VideoSampler/VideoSampler-Bridging-Header.h
@@ -5,4 +5,6 @@
 #import <DBChooser/DBChooser.h>
 #import <DBChooser/DBChooserResult.h>
 
-#import <opencv2/opencv.hpp>
+// #import <opencv2/opencv.hpp> - doesn't work in Swift
+// https://developer.apple.com/library/ios/documentation/Swift/Conceptual/BuildingCocoaApps/index.html
+// we need a bridge
